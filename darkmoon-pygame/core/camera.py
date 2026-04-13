b4583c778cf7da6404b990c92ae50c2a15492176
egaaspraim03-ai <egaaspraim03@gmail.com>
2026-04-13 16:09:33 +0300

    Delete darkmoon-pygame directory

--- darkmoon-pygame/core/camera.py
+++
@@ -1,8 +0,0 @@
-class Camera:
-    def __init__(self):
-        self.offset_x = 0
-        self.offset_y = 0
-
-    def update(self, player):
-        self.offset_x = player.rect.x - 400
-        self.offset_y = player.rect.y - 300
