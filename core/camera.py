a3099e12bf21f0399afd24b14dfa6802c41890c4
egaaspraim03-ai <egaaspraim03@gmail.com>
2026-04-13 16:15:32 +0300

    Create camera.py

---
+++ core/camera.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Camera:
+    def __init__(self):
+        self.offset_x = 0
+        self.offset_y = 0
+
+    def update(self, player):
+        self.offset_x = player.rect.x - 400
+        self.offset_y = player.rect.y - 300
